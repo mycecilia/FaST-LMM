597c35b98d7df0e379f59ff30c77eb6109c80003
Chris Widmer <chwidmer@microsoft.com>
2014-11-13 08:36:32 -0800

    incremented minor version number

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ ext.append(Extension("pysnptools.pysnptools.snpreader.wrap_matrix_subset", ["pys
 #python setup.py sdist bdist_wininst upload
 setup(
     name='fastlmm',
-    version='0.2.0',
+    version='0.2.1',
     description='Fast GWAS',
     long_description=readme(),
     keywords='gwas bioinformatics LMMs MLMs',
